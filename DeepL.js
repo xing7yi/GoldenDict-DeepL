d17972b95be3cea28422f64dec3d56fc0703d0b7
xing7yi <xing7yi@outlook.com>
2025-07-10 19:55:01 +0900

    feat: add environment variable check and enhance output formatting

--- DeepL.js
+++ DeepL.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
 require("dotenv").config({path: __dirname + "/.env"});
 
+if (!process.env.DEEPL_AUTH_KEY) {
+  console.error("Please set DEEPL_AUTH_KEY in .env file");
+  process.exit(1);
+}
+
+
 // Get command line arguments
 const arg = process.argv.slice(2)[0]; // The real command line arguments start from the third element
 
@@ -44,9 +50,14 @@ if (isChinese || input.split(" ").length > 1) {
       const text = translate_data.text;
       const character_count = usage_res.data.character_count;
       const character_limit = usage_res.data.character_limit;
-      console.log(
-        `[${detected_source_language}] ${input} <hr> [${target_lang}] ${text}`
-      );
+      console.log(`
+        <div style="color: #a0806d; background-color: #f8f4ef; text-align: justify; padding: 5px;">
+          ${input}
+          <div style="color:rgb(0, 0, 0); font-family: "PingFang SC"; margin-top: 5px;">
+            ${text}
+          </div>
+        </div>
+      `);
       console.log(`
         <div style="text-align: end;padding: 5px 5px 0;">Usage: ${character_count}/${character_limit}</div>
       `);
